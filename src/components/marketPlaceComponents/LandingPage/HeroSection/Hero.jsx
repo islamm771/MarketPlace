61c2094d9e3d450252953d013462d0bfe010da29
islam ibrahim <islamronaldo49@gmail.com>
2024-06-09 10:54:30 +0300

    modify classified form

--- src/components/marketPlaceComponents/LandingPage/HeroSection/Hero.jsx
+++ src/components/marketPlaceComponents/LandingPage/HeroSection/Hero.jsx
@@ -1,15 +1,20 @@
-import React, { useState } from 'react'
-import video_01 from "../../../../assests/marketplace/landing/1.mp4"
-import "./Hero.css"
+import React, { useState } from "react";
+import video_01 from "../../../../assests/marketplace/landing/1.mp4";
+import "./Hero.css";
 
-const Hero = ({children , video}) => {
-  
+const Hero = ({ children, video,title , svg }) => {
+  const tab = title ? (
+    <p className="active btn-tabHeroFrom !py-[15px] font-[700]">
+      {svg}
+      {title}
+    </p>
+  ) :null
   return (
     <div className="landing-hero-header">
       <div className="marketplace-container">
         {/* HTML5 VIDEO */}
         <video autoPlay muted loop id="myVideo">
-          <source src={ video ? video : video_01 } type="video/mp4" />
+          <source src={video ? video : video_01} type="video/mp4" />
         </video>
         <div className="landing-hero-header-title mb-[50px]">
           <h1 className="text-white text-[40px] mb-[10px]">Hi There!</h1>
@@ -18,11 +23,11 @@ const Hero = ({children , video}) => {
           </p>
         </div>
         <div className="landing-hero-form">
-          {children && <>{children}</>}
-        </div>
+          {children && <> {tab} {children}</>}
+          </div>
       </div>
     </div>
   );
-}
+};
 
-export default Hero
+export default Hero;
